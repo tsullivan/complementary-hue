3ef89ea88f7b4cfed96483476fecf88acdea156a
Timothy Sullivan <tsullivan@paypal.com>
2014-11-06 16:03:21 -0700

    implement cli script

--- cli.js
+++ cli.js
@@ -2,15 +2,23 @@
 
 'use strict';
 
-var comph = require('./comph');
+(function () {
+	var comph = require('./lib/comph');
 
-var userArgs = process.argv;
-var searchParam = userArgs[2];
+	var userArgs = process.argv;
+	var inputHue = userArgs[2],
+		resultHue;
 
-if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1 || searchParam === undefined) {
-    return console.log('cli help');
-}
+	if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1 || inputHue === undefined) {
+		console.log('Usage: comph <input hue: 0-359>');
+		return;
+	}
 
-if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
-    return console.log(require('./package').version);
-}
+	if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
+		console.log(require('./package').version);
+		return;
+	}
+
+	resultHue = comph.complementaryHue(inputHue);
+	console.log('Given an input hue of %d, the complementary hue is %d', inputHue, resultHue);
+}());
